2fbe6af2558f18204837bc8a38b368d23971efb8
h2jinee <wjsgmlwls97@gmail.com>
2025-01-20 00:52:50 +0900

    [Refactor] findMeetingWithMemberCount 메서드 member 패키지로 이동 (#46)

--- src/main/kotlin/com/ringgo/domain/member/repository/MemberRepositoryCustom.kt
+++ src/main/kotlin/com/ringgo/domain/member/repository/MemberRepositoryCustom.kt
@@ -1,5 +1,8 @@
 package com.ringgo.domain.member.repository
 
-interface MemberRepositoryCustom {
+import com.ringgo.domain.meeting.dto.MeetingWithMemberCount
+import java.util.*
 
+interface MemberRepositoryCustom {
+    fun findMeetingWithMemberCount(userId: UUID): List<MeetingWithMemberCount>
 }
