b7619869af6ac8a9e87619471ce0d0fb1a5bdc5a
h2jinee <wjsgmlwls97@gmail.com>
2025-01-19 22:15:53 +0900

    [Refactor] API 응답 구조 개선 (#46)

--- src/main/kotlin/com/ringgo/domain/meeting/controller/MeetingController.kt
+++ src/main/kotlin/com/ringgo/domain/meeting/controller/MeetingController.kt
@@ -1,6 +1,5 @@
 package com.ringgo.domain.meeting.controller
 
-import com.ringgo.common.dto.CommonResponse
 import com.ringgo.domain.meeting.dto.MeetingDto
 import com.ringgo.domain.meeting.service.MeetingService
 import com.ringgo.domain.user.entity.User
@@ -8,7 +7,6 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 import jakarta.validation.Valid
@@ -21,30 +19,29 @@ class MeetingController(
     private val meetingService: MeetingService
 ) {
     @Operation(summary = "모임 생성", description = "새로운 모임을 생성합니다.")
-    @ApiResponses(value = [
-        ApiResponse(responseCode = "201", description = "모임 생성 성공"),
-        ApiResponse(responseCode = "400", description = "잘못된 요청"),
-        ApiResponse(responseCode = "401", description = "인증되지 않은 사용자")
-    ])
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "201", description = "모임 생성 성공"),
+            ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            ApiResponse(responseCode = "401", description = "인증되지 않은 사용자")
+        ]
+    )
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(
         @Valid @RequestBody request: MeetingDto.Create.Request,
         @AuthenticationPrincipal user: User
-    ): ResponseEntity<CommonResponse<MeetingDto.Create.Response>> {
-        val response = meetingService.create(request, user)
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .body(CommonResponse.created(response, "모임이 성공적으로 생성되었습니다."))
+    ): MeetingDto.Create.Response {
+        return meetingService.create(request, user)
     }
 
     @Operation(summary = "모임 목록 조회", description = "사용자가 가입한 모든 모임 목록을 조회합니다.")
-    @ApiResponses(value = [
-        ApiResponse(responseCode = "200", description = "모임 목록 조회 성공")
-    ])
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "모임 목록 조회 성공")
+        ]
+    )
     @GetMapping
-    fun getMyMeeting(
-        @AuthenticationPrincipal user: User
-    ): ResponseEntity<CommonResponse<List<MeetingDto.Get.Response>>> {
-        val response = meetingService.getMyMeeting(user)
-        return ResponseEntity.ok(CommonResponse.success(response))
-    }
+    fun getMyMeeting(@AuthenticationPrincipal user: User) =
+        meetingService.getMyMeeting(user)
 }
