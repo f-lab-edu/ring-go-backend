19e080e610a896571798ef32a143805c75804489
h2jinee <wjsgmlwls97@gmail.com>
2025-02-16 23:05:25 +0900

    [Refactor] Repository 사용 안 하는 코드 삭제 (#56)

--- src/main/kotlin/com/ringgo/domain/expense/repository/ExpenseRepository.kt
+++ src/main/kotlin/com/ringgo/domain/expense/repository/ExpenseRepository.kt
@@ -3,10 +3,7 @@ package com.ringgo.domain.expense.repository
 import com.ringgo.domain.expense.entity.Expense
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface ExpenseRepository : JpaRepository<Expense, Long> {
-    fun findByActivityId(activityId: Long): List<Expense>
-    fun findByCreatorId(creatorId: UUID): List<Expense>
 }
