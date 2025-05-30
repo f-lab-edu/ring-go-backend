819935e040350b42eb755c8f477331c413215081
h2jinee <h2jinee@gmail.com>
2025-05-30 14:54:04 +0900

    [fix] Spring Data Redis 설정 및 UserRepository 추가
    - Redis 리포지토리 스캔 비활성화로 JPA 리포지토리와의 충돌 방지
    - UserRepository JpaRepository 인터페이스 구현 추가

--- src/main/kotlin/com/ringgo/common/config/redis/RedisConfig.kt
+++ src/main/kotlin/com/ringgo/common/config/redis/RedisConfig.kt
@@ -1,1 +1,13 @@
- 
+package com.ringgo.common.config.redis
+
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+
+@Configuration
+@EnableRedisRepositories(basePackages = []) // Redis 리포지토리를 사용하지 않음
+@Import(value = [RedisRepositoriesAutoConfiguration::class])
+class RedisConfig {
+    // Redis는 캐싱 용도로만 사용
+}
--- src/main/kotlin/com/ringgo/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/ringgo/domain/user/repository/UserRepository.kt
@@ -1,4 +1,10 @@
 package com.ringgo.domain.user.repository
 
-interface UserRepository {
+import com.ringgo.domain.user.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface UserRepository : JpaRepository<User, UUID> {
 }
